8e6e29540c4d744bf662e6f4dfa1942f137087ab
Aswarth <m.aswarthreddy@gmail.com>
2021-10-11 18:46:49 +0530

    turn rotary to change char and click to confirm

--- ssd1306oled.ino
+++ ssd1306oled.ino
@@ -2,42 +2,41 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#define encoderDT 0
+#define encoderCLK 1 // Interrupt
+#define encoderSW 2
+int previousDT;
+int previousCLK;
+int previousSW;
+
+
+int actualSW;
+
+bool clicked = false;
+int coordX = 0;
+int scale = 2;
+int dir = 0;
+int encoderPos = 0;
+int previousEncoderPos = 0;
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// The pins for I2C are defined by the Wire-library. 
-// On an arduino UNO:       A4(SDA), A5(SCL)
-// On an arduino MEGA 2560: 20(SDA), 21(SCL)
-// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
-
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
-static const unsigned char PROGMEM logo_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
 
 void setup() {
   Serial.begin(9600);
+  ////////rotaryEncoder//////////
+  pinMode(encoderDT, INPUT_PULLUP);
+  pinMode(encoderCLK, INPUT_PULLUP);
+  pinMode(encoderSW, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoderCLK), doEncoder, CHANGE);
+  previousDT = digitalRead(encoderDT);
+  previousCLK = digitalRead(encoderCLK);
+  previousSW = digitalRead(encoderSW);
+  ////////rotaryEncoder//////////
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -46,34 +45,68 @@ void setup() {
   }
 
   display.clearDisplay();
-
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
-  display.drawFastVLine(15, 15, 15, SSD1306_WHITE);
-  display.drawLine(15, 15, 15, 30, SSD1306_WHITE);
   display.display();
 }
 
 void loop() {
+    
+  actualSW = digitalRead(encoderSW); // Without debouncing
+  if (actualSW == LOW && previousSW == HIGH) //check for click
+   {
+     clicked = !clicked;
+     if (clicked)
+     {
+        coordX += 6;
+     }
+   }
+  previousSW = digitalRead(encoderSW);
+
+  dir = rotaryDir();
+  if(dir != 0)
+    {
+      nextCharacter();
+      Serial.println(dir);
+      Serial.println(encoderPos);
+    }
 }
 
-void testdrawchar(void) {
-  display.clearDisplay();
-
+void nextCharacter() {
   display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0);     // Start at top-left corner
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-
-  // Not all the characters will fit on the display. This is normal.
-  // Library will draw what it can and the rest will be clipped.
-  for(int16_t i=0; i<256; i++) {
-    if(i == '\n') display.write(' ');
-    else          display.write(i);
-  }
-
+  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK); // Draw white text
+  display.setCursor(coordX, 0);
+  display.write(encoderPos);
   display.display();
-  delay(2000);
+}
+
+//Rotary Encoder Logic
+int rotaryDir()
+{
+  dir = previousEncoderPos - encoderPos;
+  previousEncoderPos = encoderPos;
+  delay(40);
+  return dir;
+}
+
+void doEncoder() //rotary encoder interupt
+{
+int actualCLK = digitalRead(encoderCLK);
+int actualEncoderDT = digitalRead(encoderDT);
+
+if ((actualCLK == 1) and (previousCLK == 0))
+{
+if (actualEncoderDT == 1){
+encoderPos--;}
+else{
+encoderPos++;}
+}
+
+if ((actualCLK == 0) and (previousCLK == 1))
+{
+if (actualEncoderDT == 1){
+encoderPos++;}
+else{
+encoderPos--;}
+}
+
+previousCLK = actualCLK;
 }
